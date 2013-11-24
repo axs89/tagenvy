b30eb08606c0185ae9b4399974234d5ee114ac43
Jurgen Van de Moere <jurgen@webberid.com>
2013-11-24 12:54:10 +0100

    Added document directive with live paragraph selector

---
+++ src/js/src/tagenvy/directives/document.js
@@ -0,0 +1,28 @@
+/**
+ * @ngdoc directive
+ * @name tagenvy.directive:document
+ *
+ * @description
+ * Directive to work with document
+ */
+
+angular.module('tagenvy.directives')
+    .directive('body', ['tagenvy.config', '$rootScope', function (config, $rootScope) {
+        return {
+            restrict: 'E',
+            link: function (scope, iElement, iAttrs) {
+
+                // Broadcast paragraph clicks using live selector so it also
+                // includes paragraphs that are added dynamically in the DOM
+                // after the page initially loaded
+
+                // This will require the full jQuery library as the jqLite does
+                // not support live selectors
+
+                $(document).on('click', 'p', function(){
+                    $rootScope.$broadcast('tagenvy:document:p:click');
+                });
+
+            }
+        };
+    }]);
