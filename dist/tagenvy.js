90178ce4f25450b83c87cf864e0b730c0a9bc018
Jurgen Van de Moere <jurgen@webberid.com>
2013-11-24 12:55:20 +0100

    Updated build

--- dist/tagenvy.js
+++ dist/tagenvy.js
@@ -116,6 +116,33 @@ angular.module('tagenvy.directives')
         };
     }]);/**
  * @ngdoc directive
+ * @name tagenvy.directive:document
+ *
+ * @description
+ * Directive to work with document
+ */
+
+angular.module('tagenvy.directives')
+    .directive('body', ['tagenvy.config', '$rootScope', function (config, $rootScope) {
+        return {
+            restrict: 'E',
+            link: function (scope, iElement, iAttrs) {
+
+                // Broadcast paragraph clicks using live selector so it also
+                // includes paragraphs that are added dynamically in the DOM
+                // after the page initially loaded
+
+                // This will require the full jQuery library as the jqLite does
+                // not support live selectors
+
+                $(document).on('click', 'p', function(){
+                    $rootScope.$broadcast('tagenvy:document:p:click');
+                });
+
+            }
+        };
+    }]);/**
+ * @ngdoc directive
  * @name tagenvy.directive:p
  *
  * @description
@@ -136,68 +163,4 @@ angular.module('tagenvy.directives')
 
             }
         };
-    }]);/**
- * @ngdoc object
- * @name service:logger
- * @requires $log
- *
- * @description
- * Simple service for logging. Default implementation writes the message
- * into the browser's console (if present).
- *
- * The main purpose of this service is to simplify debugging and troubleshooting.
- *
- * This service proxies all methods to the AngularJS $log service and offers
- * one extra method: dir.
- *
- * The dir method can be used to hierarchically display object properties.
- *
- * @example
- <example>
- <file name="script.js">
- function LogCtrl($scope, logger) {
-         $scope.logger = logger;
-         $scope.message = 'Hello World!';
-       }
- </file>
- <file name="index.html">
- <div ng-controller="LogCtrl">
- <p>Reload this page with open console, enter text and hit the log button...</p>
- Message:
- <input type="text" ng-model="message"/>
- <button ng-click="logger.log(message)">log</button>
- <button ng-click="logger.warn(message)">warn</button>
- <button ng-click="logger.info(message)">info</button>
- <button ng-click="logger.error(message)">error</button>
- <button ng-click="logger.dir(message)">dir</button>
- </div>
- </file>
- </example>
- */
-
-angular.module('tagenvy.services')
-    .factory('logger', ['$log', function ($log) {
-
-        // Create service
-        var service = {};
-
-        // Proxy regular methods to $log
-        angular.forEach(['log', 'info', 'warn', 'error'], function (method) {
-            service[method] = function () {
-                return $log[method](arguments);
-            };
-        });
-
-
-        // Add dir method to hierarchically display objects
-        service.dir = function (obj, title) {
-            if (window.console) {
-                if (angular.isDefined(title)) {
-                    $log.info(title + ':');
-                }
-                window.console.dir(obj);
-            }
-        };
-
-        return service;
     }]);})(window, document);
