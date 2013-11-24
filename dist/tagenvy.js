b5dabaeddb144129cd0bde721c62a73b029968f1
Jurgen Van de Moere <jurgen@webberid.com>
2013-11-24 10:57:33 +0100

    Updated build

--- dist/tagenvy.js
+++ dist/tagenvy.js
@@ -95,10 +95,31 @@ angular.element(document).ready(function() {
     console.dir(window.tagenvy);
 });/**
  * @ngdoc directive
+ * @name tagenvy.directive:body
+ *
+ * @description
+ * Directive to work with body element
+ */
+
+angular.module('tagenvy.directives')
+    .directive('body', ['tagenvy.config', '$rootScope', '$location', function (config, $rootScope, $location) {
+        return {
+            restrict: 'E',
+            link: function (scope, iElement, iAttrs) {
+
+                // Broadcast init event when the body is ready
+                angular.element(iElement).ready(function(){
+                    $rootScope.$broadcast('tagenvy:body:init', $location);
+                });
+
+            }
+        };
+    }]);/**
+ * @ngdoc directive
  * @name tagenvy.directive:p
  *
  * @description
- * Test directive to work with paragraphs on example page
+ * Directive to work with paragraph elements
  */
 
 angular.module('tagenvy.directives')
@@ -107,6 +128,7 @@ angular.module('tagenvy.directives')
             restrict: 'E',
             link: function (scope, iElement, iAttrs) {
 
+                // Broadcast click events
                 iElement.bind('click', function(){
                     console.log('Broadcast tagenvy:p:click event');
                     $rootScope.$broadcast('tagenvy:p:click', iElement);
@@ -114,4 +136,68 @@ angular.module('tagenvy.directives')
 
             }
         };
+    }]);/**
+ * @ngdoc object
+ * @name service:logger
+ * @requires $log
+ *
+ * @description
+ * Simple service for logging. Default implementation writes the message
+ * into the browser's console (if present).
+ *
+ * The main purpose of this service is to simplify debugging and troubleshooting.
+ *
+ * This service proxies all methods to the AngularJS $log service and offers
+ * one extra method: dir.
+ *
+ * The dir method can be used to hierarchically display object properties.
+ *
+ * @example
+ <example>
+ <file name="script.js">
+ function LogCtrl($scope, logger) {
+         $scope.logger = logger;
+         $scope.message = 'Hello World!';
+       }
+ </file>
+ <file name="index.html">
+ <div ng-controller="LogCtrl">
+ <p>Reload this page with open console, enter text and hit the log button...</p>
+ Message:
+ <input type="text" ng-model="message"/>
+ <button ng-click="logger.log(message)">log</button>
+ <button ng-click="logger.warn(message)">warn</button>
+ <button ng-click="logger.info(message)">info</button>
+ <button ng-click="logger.error(message)">error</button>
+ <button ng-click="logger.dir(message)">dir</button>
+ </div>
+ </file>
+ </example>
+ */
+
+angular.module('tagenvy.services')
+    .factory('logger', ['$log', function ($log) {
+
+        // Create service
+        var service = {};
+
+        // Proxy regular methods to $log
+        angular.forEach(['log', 'info', 'warn', 'error'], function (method) {
+            service[method] = function () {
+                return $log[method](arguments);
+            };
+        });
+
+
+        // Add dir method to hierarchically display objects
+        service.dir = function (obj, title) {
+            if (window.console) {
+                if (angular.isDefined(title)) {
+                    $log.info(title + ':');
+                }
+                window.console.dir(obj);
+            }
+        };
+
+        return service;
     }]);})(window, document);
