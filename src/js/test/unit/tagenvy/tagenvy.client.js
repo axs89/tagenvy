406e9efbe9bf1f41accf0a4ccbd013bb3d153f77
Jurgen Van de Moere <jurgen@webberid.com>
2013-11-24 16:32:12 +0100

    Added unit test that checks for tagenvy:ready event when tagenvy.client is loaded

---
+++ src/js/test/unit/tagenvy/tagenvy.client.js
@@ -0,0 +1,35 @@
+'use strict';
+
+describe('tagenvy.directives.body', function() {
+
+    var $rootScope,
+        rootScopeBroadcastSpy;
+
+    beforeEach(module('tagenvy.client'));
+
+    beforeEach(inject(function($compile, _$rootScope_, _$injector_) {
+
+        // Attach injector and rootscope to tagenvy
+        window.tagenvy.$injector = _$injector_;
+        window.tagenvy.$rootScope = _$rootScope_;
+
+        $rootScope = _$rootScope_;
+
+        // Create spy
+        rootScopeBroadcastSpy = spyOn($rootScope, '$broadcast');
+
+        // Compile markup
+        $compile('<html><bidy><p></p></bidy></html>')($rootScope);
+
+        // Perform digest cycle
+        $rootScope.$digest();
+
+        // Perform postBootstrap
+        window.tagenvy.postBootstrap();
+    }));
+
+    it('should broadcast a tagenvy:ready event', function() {
+        expect(rootScopeBroadcastSpy).toHaveBeenCalledWith('tagenvy:ready');
+    });
+
+});
