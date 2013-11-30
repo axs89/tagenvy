a4eef8c9f8706f66085a9107a6a9d42eb210d6c6
Jurgen Van de Moere <jurgen@webberid.com>
2013-11-30 17:51:12 +0100

    Updated body directive to simulate gist

--- src/js/src/tagenvy/directives/body.js
+++ src/js/src/tagenvy/directives/body.js
@@ -12,9 +12,38 @@ angular.module('tagenvy.directives')
             restrict: 'E',
             link: function (scope, iElement, iAttrs) {
 
-                // Broadcast init event when the body is ready
+                // Create array of class names
+                var classNames = [];
+                if (iAttrs.class){
+                    classNames = iAttrs.class.split(/\s+/);
+                }
+
+                var bodyId = iAttrs.id || void 0;
+
+                // Broadcast events when the body is ready
                 angular.element(iElement).ready(function(){
+
+                    if (config.debug) console.log('tagenvy:body:init');
                     $rootScope.$broadcast('tagenvy:body:init', $location);
+
+                    if (config.debug) console.log('tagenvy:common:init');
+                    $rootScope.$broadcast('tagenvy:common:init', $location);
+
+                    angular.forEach(classNames, function(className){
+
+                        if (config.debug) console.log('tagenvy:' + className + ':init');
+                        $rootScope.$broadcast('tagenvy:' + className + ':init', $location);
+
+                        if(bodyId){
+                            if (config.debug) console.log('tagenvy:' + className + ':' + bodyId);
+                            $rootScope.$broadcast('tagenvy:' + className + ':' + bodyId, $location);
+                        }
+
+                    });
+
+                    if (config.debug) console.log('tagenvy:common:finalize');
+                    $rootScope.$broadcast('tagenvy:common:finalize', $location);
+
                 });
 
             }
