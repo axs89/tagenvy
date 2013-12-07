0b8d8dd8a996708a9aa28c24094a35523be8c621
Jurgen Van de Moere <jurgen@webberid.com>
2013-12-07 08:22:37 +0100

    Added unit tests for testing modular structure of tagenvy.common

--- src/js/test/unit/tagenvy/tagenvy.common.js
+++ src/js/test/unit/tagenvy/tagenvy.common.js
@@ -1,29 +1,32 @@
 'use strict';
 
-describe('tagenvy.common module', function() {
+describe('tagenvy.common module', function () {
 
     var module,
         dependencies;
 
     dependencies = [];
 
-    var hasModule = function(module) {
+    var hasModule = function (module) {
         return dependencies.indexOf(module) >= 0;
     };
 
-    beforeEach(function() {
+    beforeEach(function () {
 
         // Get module
         module = angular.module('tagenvy.common');
         dependencies = module.requires;
     });
 
-    it('should load directives module', function() {
+    it('should load the tagenvy module', function () {
+        expect(hasModule('tagenvy')).toBeTruthy();
+    });
+
+    it('should load the tagenvy.common.directives module', function () {
         expect(hasModule('tagenvy.common.directives')).toBeTruthy();
     });
 
-    it('should load common module', function() {
+    it('should load the tagenvy.common.services module', function () {
         expect(hasModule('tagenvy.common.services')).toBeTruthy();
     });
-
 });
